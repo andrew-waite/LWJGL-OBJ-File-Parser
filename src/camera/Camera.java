79febee8d07515a040e9552f19cf8ad4856e25e3
Andrew Waite <drewrockz01@gmail.com>
2017-01-27 18:55:27 +1100

    Added getter and setter methods for Camera class

--- src/camera/Camera.java
+++ src/camera/Camera.java
@@ -5,6 +5,12 @@ import org.lwjgl.util.vector.Vector3f;
 public class Camera
 {
     private double speed = 0.5f;
+    private double angle = 0.0f;
+    
+    private double deltaAngle = 0.0f;
+    private double deltaMove = 0;
+    private int xOrigin = -1;
+    private double mouseDirection = 0.0;
 
     private Vector3f position;
     private Vector3f rotation;
@@ -40,4 +46,76 @@ public class Camera
         position.y += positive ? cy : -cy;
         position.z += positive ? cz : -cz;
     } 
+    
+    public double getAngle()
+    {
+        return this.angle;
+    }
+
+    public Vector3f getRotation()
+    {
+        return this.rotation;
+    }
+    
+    public Vector3f getPosition()
+    {
+        return this.position;
+    }
+
+    public double getMouseDirection()
+    {
+        return this.mouseDirection;
+    }
+
+    public double getDeltaAngle()
+    {
+        return this.deltaAngle;
+    }
+
+    public double getDeltaMove()
+    {
+        return this.deltaMove;
+    }
+
+    public int getXOrigin()
+    {
+        return this.xOrigin;
+    }
+
+    public void setAngle(float angle)
+    {
+        this.angle = angle;
+    }
+
+   public void setRotation(Vector3f vector)
+   {
+       this.rotation = vector;
+   }
+
+    public void setPosition(Vector3f position)
+    {
+        this.position = position;
+    }
+
+    public void setMouseDirection(float mouseDirection)
+    {
+        this.mouseDirection = mouseDirection;
+    }
+
+    public void setDeltaAngle(float deltaAngle)
+    {
+        this.deltaAngle = deltaAngle;
+    }
+
+    public void setDeltaMove(float deltaMove)
+    {
+        this.deltaMove = deltaMove;
+    }
+
+    public void setXOrigin(int xOrigin)
+    {
+        this.xOrigin = xOrigin;
+    }
+    
+    
 }
