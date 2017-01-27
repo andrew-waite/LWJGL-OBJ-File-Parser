45e56704a508803e0d6fefdd97f517fd702eccbb
Andrew Waite <drewrockz01@gmail.com>
2017-01-27 18:37:40 +1100

    Added straffing and position computation to Camera Class
    Added a compute pos method to calculate the new position of the camera
    Adde a crossproduct method to move the camera left and right
    horizontally across the screen to the world.

--- src/camera/Camera.java
+++ src/camera/Camera.java
@@ -15,5 +15,29 @@ public class Camera
         rotation = new Vector3f(0,0,0);
     }
     
-    
+    public void computePos(float deltaMove) 
+    {
+
+        position.x += deltaMove * rotation.x * 1.0f;
+        position.z += deltaMove * rotation.z * 1.0f;
+        position.y += deltaMove * rotation.y * 1.0f;
+    }
+
+    public void crossProduct(boolean positive, float ax, float ay, float az, float bx, float by, float bz)
+    {
+        float strafeSpeed = 0.2f;
+        
+        float cx = 0.0f, cy, cz;
+        cx = ay * bz - az * by;
+        cy = az * bx - ax * bz;
+        cz = ax * by - ay * bx;
+
+        cx *= strafeSpeed;
+        cy *= strafeSpeed;
+        cz *= strafeSpeed;
+        
+        position.x += positive ? cx : -cx;
+        position.y += positive ? cy : -cy;
+        position.z += positive ? cz : -cz;
+    } 
 }
