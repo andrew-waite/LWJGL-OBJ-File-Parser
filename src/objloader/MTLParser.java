46aac2e5bc00446b0e12bc788492db3471f9df20
Andrew Waite <drewrockz01@gmail.com>
2017-02-07 23:23:16 +1100

    Variables added for MTLParser
    Added ambient, specular,and diffuse vairables to store colour properties
    of each kind of light.

--- src/objloader/MTLParser.java
+++ src/objloader/MTLParser.java
@@ -1,7 +1,13 @@
 package objloader;
 
+import utilities.Vec3;
+
 public class MTLParser
 {
+    private Vec3 ambient;
+    private Vec3 diffuse;
+    private Vec3 specular;
+    
     public MTLParser()
     {
         
