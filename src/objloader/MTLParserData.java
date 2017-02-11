faee36a52a2f8b55b4d3e6a244e3a5cf8bef3631
Andrew Waite <drewrockz01@gmail.com>
2017-02-11 19:52:50 +1100

    Added getter and constructor methods to MTLParserData

--- src/objloader/MTLParserData.java
+++ src/objloader/MTLParserData.java
@@ -1,6 +1,32 @@
 package objloader;
 
+import utilities.Vec3;
+
 public class MTLParserData
 {
-
+    private Vec3 ambient;
+    private Vec3 diffuse;
+    private Vec3 specular;
+    
+    public MTLParserData(Vec3 ambient, Vec3 diffuse, Vec3 specular)
+    {
+        this.ambient = ambient;
+        this.diffuse = diffuse;
+        this.specular = specular;
+    }
+    
+    public Vec3 getAmbient()
+    {
+        return this.ambient;
+    }
+    
+    public Vec3 getDiffuse()
+    {
+        return this.diffuse;
+    }
+    
+    public Vec3 getSpecular()
+    {
+        return this.specular;
+    }
 }
